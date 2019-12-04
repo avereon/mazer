5274212eb3dba0d525595491de0cc33542eed003
Mark Soderquist <mark@soderquist.net>
2019-12-03 19:23:50 -0700

    Add MazerIcon

--- source/main/java/com/avereon/mazer/Mazer.java
+++ source/main/java/com/avereon/mazer/Mazer.java
@@ -4,4 +4,24 @@ import com.avereon.xenon.Mod;
 
 public class Mazer extends Mod {
 
+	@Override
+	public void register() {
+		super.register();
+		getProgram().getIconLibrary().register( "mazer", MazerIcon.class );
+	}
+
+	@Override
+	public void startup() {
+		super.startup();
+	}
+
+	@Override
+	public void shutdown() {
+		super.shutdown();
+	}
+
+	@Override
+	public void unregister() {
+		super.unregister();
+	}
 }
