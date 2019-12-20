a4e821f24769f2db1207d7e552b5df0ce9fe26ad
Mark Soderquist <soderquistmv@churchofjesuschrist.org>
2019-12-20 09:45:40 -0700

    Work on mod resource bundles

--- source/main/java/com/avereon/mazer/MazeTool.java
+++ source/main/java/com/avereon/mazer/MazeTool.java
@@ -1,13 +1,21 @@
 package com.avereon.mazer;
 
+import com.avereon.xenon.OpenToolRequestParameters;
 import com.avereon.xenon.ProgramProduct;
 import com.avereon.xenon.asset.Asset;
 import com.avereon.xenon.tool.ProgramTool;
+import com.avereon.xenon.workpane.ToolException;
 
 public class MazeTool extends ProgramTool {
 
 	public MazeTool( ProgramProduct product, Asset asset ) {
 		super( product, asset );
+	}
+
+	@Override
+	protected void assetReady( OpenToolRequestParameters parameters ) throws ToolException {
+		super.assetReady( parameters );
+		setTitle( getAsset().getName() );
 	}
 
 }
