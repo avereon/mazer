42082a83c557cf198649980ce717fd9c90f73159
Mark Soderquist <soderquistmv@churchofjesuschrist.org>
2020-02-05 09:14:15 -0700

    Update asset type key

--- source/main/java/com/avereon/mazer/MazeAssetType.java
+++ source/main/java/com/avereon/mazer/MazeAssetType.java
@@ -8,9 +8,16 @@ import com.avereon.xenon.asset.AssetType;
 
 public class MazeAssetType extends AssetType {
 
+	private static final String MEDIA_TYPE = "application/vnd.avereon.mazer.maze";
+
 	public MazeAssetType( Product product ) {
 		super( product, "mazer" );
 		setDefaultCodec( new MazeCodec( product ) );
+	}
+
+	@Override
+	public String getKey() {
+		return MEDIA_TYPE;
 	}
 
 	@Override
