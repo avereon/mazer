994ef9419e2ec1988a361e8650287b7ae3fc3099
Mark Soderquist <mark@soderquist.net>
2020-03-24 15:35:39 -0600

    AssetType now requires a ProgramProduct

--- source/main/java/com/avereon/mazer/MazeAssetType.java
+++ source/main/java/com/avereon/mazer/MazeAssetType.java
@@ -1,7 +1,7 @@
 package com.avereon.mazer;
 
-import com.avereon.product.Product;
 import com.avereon.xenon.Program;
+import com.avereon.xenon.ProgramProduct;
 import com.avereon.xenon.asset.Asset;
 import com.avereon.xenon.asset.AssetException;
 import com.avereon.xenon.asset.AssetType;
@@ -10,7 +10,7 @@ public class MazeAssetType extends AssetType {
 
 	private static final String MEDIA_TYPE = "application/vnd.avereon.mazer.maze";
 
-	public MazeAssetType( Product product ) {
+	public MazeAssetType( ProgramProduct product ) {
 		super( product, "mazer" );
 		setDefaultCodec( new MazeCodec( product ) );
 	}
