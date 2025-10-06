2276f867d1dbcba1f5cca319aa380fac5b589f62
Mark Soderquist <mark@soderquist.net>
2025-10-06 12:57:34 -0600

    Rename `AssetException` to `ResourceException`.
    - Updated exception type across various classes and methods for consistency.
    - Adjusted error handling logic to align with the new exception type.

--- source/main/java/com/avereon/mazer/MazeAssetType.java
+++ source/main/java/com/avereon/mazer/MazeAssetType.java
@@ -3,7 +3,7 @@ package com.avereon.mazer;
 import com.avereon.xenon.Xenon;
 import com.avereon.xenon.XenonProgramProduct;
 import com.avereon.xenon.asset.Asset;
-import com.avereon.xenon.asset.exception.AssetException;
+import com.avereon.xenon.asset.exception.ResourceException;
 import com.avereon.xenon.asset.AssetType;
 
 public class MazeAssetType extends AssetType {
@@ -19,12 +19,12 @@ public class MazeAssetType extends AssetType {
 	}
 
 	@Override
-	public boolean assetNew( Xenon program, Asset asset ) throws AssetException {
+	public boolean assetNew( Xenon program, Asset asset ) throws ResourceException {
 		return super.assetNew( program, asset );
 	}
 
 	@Override
-	public boolean assetOpen( Xenon program, Asset asset ) throws AssetException {
+	public boolean assetOpen( Xenon program, Asset asset ) throws ResourceException {
 		asset.setModel( new Maze() );
 		return true;
 	}
